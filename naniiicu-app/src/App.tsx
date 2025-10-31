d856ed7625d91b44efd97fde08305f6912bc06ae
murilo.moura <murilomoura2005@gmail.com>
2025-10-30 21:37:40 -0300

    reverted to nginx proxy

--- naniiicu-app/src/App.tsx
+++ naniiicu-app/src/App.tsx
@@ -7,7 +7,6 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { I18nProvider } from "@/contexts/I18nProvider";
 import HubDisplay from "./components/HubDisplay";
-import RedirectHandler from "./components/RedirectHandler";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -22,7 +21,6 @@ const App = () => (
             <Route path="/h/:hubName" element={<HubDisplay />} />
             <Route path="/404/not-found" element={<NotFound />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="/:shortName" element={<RedirectHandler />} />
             <Route path="*" element={<Index />} />
           </Routes>
         </BrowserRouter>
